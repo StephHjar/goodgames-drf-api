ca0145b1a5bfceb458938c84006b5bfbb62f548c
Steph Hjartarson <stephanie.hja@gmail.com>
2023-02-18 12:19:42 +0000

    Create game list view

--- games/views.py
+++ games/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Game
+from .serializers import GameSerializer
 
-# Create your views here.
+
+class GameList(APIView):
+    def get(self, request):
+        games = Game.objects.all()
+        serializer = GameSerializer(
+            games, many=True, context={'request': request}
+        )
+        return Response(serializer.data)

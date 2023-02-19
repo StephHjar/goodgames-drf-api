d8ba74fa35122c8a07a6468e71c0b52f1b53ea81
Steph Hjartarson <stephanie.hja@gmail.com>
2023-02-19 17:09:11 +0000

    Create like model

---
+++ likes/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
+from comments.models import Comment
+from games.models import Game
+
+
+class Like(models.Model):
+    """
+    Comment model, related to User and Post.
+    Code from Code Institute's Django REST Framework walkthrough.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, related_name='likes', on_delete=models.CASCADE
+        )
+    comment = models.ForeignKey(
+        Comment, related_name='likes', on_delete=models.CASCADE
+    )
+    game = models.ForeignKey(
+        Game, related_name='likes', on_delete=models.CASCADE
+        )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = [
+            ['owner', 'post'], ['owner', 'comment'], ['owner', 'game']
+            ]
+
+    def __str__(self):
+        return f"{self.owner} {self.id}"

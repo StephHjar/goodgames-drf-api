f9a40fef19902eefdbc71592cfbb05ed1ba9a26b
Steph Hjartarson <stephanie.hja@gmail.com>
2023-02-19 17:48:59 +0000

    Refactor game, profile and post views as generic views

--- games/views.py
+++ games/views.py
@@ -1,71 +1,19 @@
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics, permissions
 from .models import Game
 from .serializers import GameSerializer
 from goodgames_drf_api.permissions import IsOwnerOrReadOnly
 
 
-class GameList(APIView):
+class GameList(generics.ListCreateAPIView):
     serializer_class = GameSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
-    ]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Game.objects.all()
 
-    def get(self, request):
-        games = Game.objects.all()
-        serializer = GameSerializer(
-            games, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = GameSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-class GameDetail(APIView):
+class GameDetail(generics.RetrieveUpdateDestroyAPIView()):
     serializer_class = GameSerializer
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        try:
-            game = Game.objects.get(pk=pk)
-            self.check_object_permissions(self.request, game)
-            return game
-        except Game.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        game = self.get_object(pk)
-        serializer = GameSerializer(
-            game, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        game = self.get_object(pk)
-        serializer = GameSerializer(
-            game, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        game = self.get_object(pk)
-        game.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    queryset = Game.objects.all()

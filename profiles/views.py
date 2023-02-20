e362a65a5dc5da39a91850f290b5482ca2ebf14a
Steph Hjartarson <stephanie.hja@gmail.com>
2023-02-20 20:29:09 +0000

    Add posts count to profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,19 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from .models import Profile
 from .serializers import ProfileSerializer
 from goodgames_drf_api.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(generics.ListAPIView):
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = ['posts_count']
 
 
 class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):

afbf3a3fb77ca31cf49a58e7d9dae6e7a965896e
Steph Hjartarson <stephanie.hja@gmail.com>
2023-02-25 10:26:10 +0000

    Add Heroku app to allowed hosts

--- goodgames_drf_api/settings.py
+++ goodgames_drf_api/settings.py
@@ -60,7 +60,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'goodgames-pp5.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'goodgames-drf-api.herokuapp.com']
 
 
 # Application definition

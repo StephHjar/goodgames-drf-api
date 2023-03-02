8fbee9ea3760904f74d927ca41dd5c94c073e408
Steph Hjartarson <stephanie.hja@gmail.com>
2023-03-02 22:06:09 +0000

    Add db_constraint back to Like model fields

--- likes/models.py
+++ likes/models.py
@@ -14,19 +14,19 @@ class Like(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(
         Post, related_name='likes', blank=True, null=True, default=None,
-        on_delete=models.CASCADE
+        db_constraint=False, on_delete=models.CASCADE
         )
     comment = models.ForeignKey(
         Comment, related_name='likes', blank=True, null=True, default=None,
-        on_delete=models.CASCADE
+        db_constraint=False, on_delete=models.CASCADE
     )
     review = models.ForeignKey(
         Review, related_name='likes', blank=True, null=True, default=None,
-        on_delete=models.CASCADE
+        db_constraint=False, on_delete=models.CASCADE
         )
     game = models.ForeignKey(
         Game, related_name='likes', blank=True, null=True, default=None,
-        on_delete=models.CASCADE
+        db_constraint=False, on_delete=models.CASCADE
     )
     created_at = models.DateTimeField(auto_now_add=True)
 

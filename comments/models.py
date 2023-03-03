9611379541a355d0768edf9491890aeae2f17cc9
Steph Hjartarson <stephanie.hja@gmail.com>
2023-03-03 11:39:52 +0000

    Fix typo in GenericForeignKey import

--- comments/models.py
+++ comments/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
 from posts.models import Post
 from likes.models import Like
 

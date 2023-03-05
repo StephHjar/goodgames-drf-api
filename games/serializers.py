e9ac57fa3e9712aec44c4d7e93e2bbd6d43aef4f
Steph Hjartarson <stephanie.hja@gmail.com>
2023-03-05 15:39:56 +0000

    Add review_id field to game serializer to avoid duplicate reviews

--- games/serializers.py
+++ games/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Game
 from likes.models import Like
+from reviews.models import Review
 
 
 class GameSerializer(serializers.ModelSerializer):
@@ -9,6 +10,7 @@ class GameSerializer(serializers.ModelSerializer):
     like_id = serializers.SerializerMethodField()
     reviews_count = serializers.ReadOnlyField()
     likes_count = serializers.ReadOnlyField()
+    review_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
@@ -36,10 +38,17 @@ class GameSerializer(serializers.ModelSerializer):
             return like.id if like else None
         return None
 
+    def get_review_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            review = Review.objects.filter(owner=user, game=obj).first()
+            return review.id if review else None
+        return None
+
     class Meta:
         model = Game
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'title', 'image',
             'description', 'like_id', 'reviews_count', 'likes_count',
-            'is_admin',
+            'is_admin', 'review_id',
         ]
